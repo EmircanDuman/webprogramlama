43d543f5480f85ff10a7de1b517385221a342b4c
Emircan Duman <emircandum10@gmail.com>
2024-04-28 14:34:07 +0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <div className="relative h-2/3 w-1/4 flex flex-col justify-center gap-16 items-center">
+        <div className="relative">
+          <span className="text-6xl font-extrabold bg-gradient-to-r select-none from-pink-500 via-red-500 to-yellow-500 bg-clip-text yp-5 text-transparent">
+            BlogSQL &nbsp;
+          </span>
+
+          <span className="underline text-red-300 font-bold text-4xl select-none">
+            Login
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
